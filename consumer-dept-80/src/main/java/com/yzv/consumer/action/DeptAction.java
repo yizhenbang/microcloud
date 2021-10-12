a3c3a841f679a8ecd77b70d8fba104a87f55cde5
YiZhenBang <1628755314@qq.com>
2021-10-12 22:04:51 +0800

    微服务消费端

--- consumer-dept-80/src/main/java/com/yzv/consumer/action/DeptAction.java
+++ consumer-dept-80/src/main/java/com/yzv/consumer/action/DeptAction.java
@@ -3,22 +3,12 @@ package com.yzv.consumer.action;
 import com.yzb.common.dto.DeptDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
-/**
- * ClassName: DeptAction
- * Description:
- * date: 2021/10/12 18:49
- *
- * @author ZhenBang-Yi
- * @version 1.0
- * @since JDK 1.8
- */
 @RestController
 @RequestMapping("/consumer/dept/*")
 public class DeptAction {
