edd8fffc9b2fc544b3b64afd512aa7849ec99e7d
YiZhenBang <1628755314@qq.com>
2021-11-22 09:47:43 +0800

    Feign连接池，转换日志

--- consumer-dept-80/src/main/java/com/yzv/consumer/SpringCloudConsumerStarter.java
+++ consumer-dept-80/src/main/java/com/yzv/consumer/SpringCloudConsumerStarter.java
@@ -1,6 +1,7 @@
 package com.yzv.consumer;
 
 import com.yzv.ribbon.config.RibbonConfig;
+import feign.Feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
