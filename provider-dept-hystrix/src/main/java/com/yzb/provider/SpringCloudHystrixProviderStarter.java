eb72680ef50c0f9f36f0e086bcc0b7cd720b6a55
YiZhenBang <1628755314@qq.com>
2021-11-23 18:17:14 +0800

    Hystrix的基本使用

---
+++ provider-dept-hystrix/src/main/java/com/yzb/provider/SpringCloudHystrixProviderStarter.java
@@ -0,0 +1,13 @@
+package com.yzb.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@SpringBootApplication
+@EnableCircuitBreaker // 开启熔断
+public class SpringCloudHystrixProviderStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudHystrixProviderStarter.class, args);
+    }
+}
