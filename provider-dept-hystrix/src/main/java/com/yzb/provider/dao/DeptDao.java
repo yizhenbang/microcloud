523fdedab880ef35547b32424c5b3a60919aaa6d
YiZhenBang <1628755314@qq.com>
2021-11-26 21:45:03 +0800

    Sentinel 初始化模板

--- provider-dept-hystrix/src/main/java/com/yzb/provider/dao/DeptDao.java
+++
@@ -1,9 +0,0 @@
-package com.yzb.provider.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.yzb.provider.vo.Dept;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DeptDao extends BaseMapper<Dept> {
-}
