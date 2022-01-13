7d0fba6018cf6550c573fa1fc1dd7cbc1b80e7ee
YiZhenBang <1628755314@qq.com>
2022-01-13 23:15:07 +0800

    自定义过滤工厂

---
+++ microGateway-9501/src/main/java/com/yzb/gateway/filter/LogGatewayFilterFactory.java
@@ -0,0 +1,33 @@
+package com.yzb.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+/**
+ * ClassName: LogGatewayFilter
+ * Description: 做日志过滤工程
+ * date: 2022/1/13 23:00
+ *
+ * @author ZhenBang-Yi
+ * @version 1.0
+ * @since JDK 1.8
+ */
+
+@Component
+@Slf4j
+public class LogGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
+    @Override
+    public GatewayFilter apply(NameValueConfig config) {
+        return ((exchange, chain) -> {
+            log.info("【NAME:{}，VALUE:{}】", config.getName(), config.getValue());
+            ServerHttpRequest request = exchange.getRequest().mutate().build();
+            log.info("【请求方法：{}】，【请求路径：{}】", request.getMethod(), request.getPath());
+            ServerWebExchange build = exchange.mutate().request(request).build();
+            return chain.filter(exchange);
+        });
+    }
+}
